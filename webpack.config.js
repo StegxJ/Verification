ff29a6751e92417b6713125efa5531407a7a83d8
StegxJ <jan.hoenen@stegx.finance>
2024-07-17 19:18:07 +0200

    Fix: Add path-browserify and update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,10 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
+  mode: 'production',
   resolve: {
     fallback: {
       "path": require.resolve("path-browserify")
